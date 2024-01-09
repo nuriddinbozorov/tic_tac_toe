63696134e0b83a4b21266d734288585a58272a55
Nuriddin Bozorov <nuriddinbozorov201@gmail.com>
2024-01-09 06:21:12 +0500

    3 va 4 - videolar

--- lib/main.dart
+++ lib/main.dart
@@ -24,11 +24,78 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String displayExOh = "";
-  void _tapped() {
+  List<String> displayExOh = ['', '', '', '', '', '', '', '', ''];
+  bool ohTurn = true; // Birinchi o'yinchi = O
+  void _tapped(int index) {
     setState(() {
-      displayExOh = '0';
+      if (ohTurn) {
+        displayExOh[index] = 'O';
+      } else {
+        displayExOh[index] = 'X';
+      }
+      ohTurn = !ohTurn;
+      checkWinner();
     });
+  }
+
+  void checkWinner() {
+    //1-qator
+    if (displayExOh[0] == displayExOh[1] &&
+        displayExOh[0] == displayExOh[2] &&
+        displayExOh[0] != '') {
+      checkWinnerDialog();
+    }
+    //2-qator
+    if (displayExOh[3] == displayExOh[4] &&
+        displayExOh[3] == displayExOh[5] &&
+        displayExOh[3] != '') {
+      checkWinnerDialog();
+    }
+    //3-qator
+    if (displayExOh[6] == displayExOh[7] &&
+        displayExOh[6] == displayExOh[8] &&
+        displayExOh[6] != '') {
+      checkWinnerDialog();
+    }
+    //1-qator
+    if (displayExOh[0] == displayExOh[3] &&
+        displayExOh[0] == displayExOh[6] &&
+        displayExOh[0] != '') {
+      checkWinnerDialog();
+    }
+    //2-qator
+    if (displayExOh[1] == displayExOh[4] &&
+        displayExOh[1] == displayExOh[7] &&
+        displayExOh[1] != '') {
+      checkWinnerDialog();
+    }
+    //3-qator
+    if (displayExOh[2] == displayExOh[5] &&
+        displayExOh[2] == displayExOh[8] &&
+        displayExOh[2] != '') {
+      checkWinnerDialog();
+    }
+    //1-dioganal
+    if (displayExOh[0] == displayExOh[4] &&
+        displayExOh[0] == displayExOh[8] &&
+        displayExOh[0] != '') {
+      checkWinnerDialog();
+    }
+    //2-dioganal
+    if (displayExOh[2] == displayExOh[4] &&
+        displayExOh[2] == displayExOh[6] &&
+        displayExOh[2] != '') {
+      checkWinnerDialog();
+    }
+  }
+
+  void checkWinnerDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(title: Text('Winner is: ' + (ohTurn ? 'X' : 'O')));
+      },
+    );
   }
 
   @override
@@ -42,12 +109,14 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
         itemBuilder: (BuildContext context, index) {
           return GestureDetector(
-            onTap: _tapped,
+            onTap: () {
+              _tapped(index);
+            },
             child: Container(
               decoration: BoxDecoration(border: Border.all(color: Colors.grey)),
               child: Center(
                 child: Text(
-                  displayExOh,
+                  displayExOh[index],
                   style: TextStyle(color: Colors.white, fontSize: 40),
                 ),
               ),
