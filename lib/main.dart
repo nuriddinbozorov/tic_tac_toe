884c509724815717adba7141b556ec98f7bd8146
Nuriddin Bozorov <nuriddinbozorov201@gmail.com>
2025-09-19 10:03:33 +0500

    Oxirgi holati

--- lib/main.dart
+++ lib/main.dart
@@ -26,12 +26,18 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   List<String> displayExOh = ['', '', '', '', '', '', '', '', ''];
   bool ohTurn = true; // Birinchi o'yinchi = O
+  int playerXscore = 0;
+  int playerOscore = 0;
+  int filledBoxes = 0;
+
   void _tapped(int index) {
     setState(() {
-      if (ohTurn) {
+      if (ohTurn && displayExOh[index] == '') {
         displayExOh[index] = 'O';
-      } else {
+        filledBoxes += 1;
+      } else if (!ohTurn && displayExOh[index] == '') {
         displayExOh[index] = 'X';
+        filledBoxes += 1;
       }
       ohTurn = !ohTurn;
       checkWinner();
@@ -43,86 +49,180 @@ class _MyHomePageState extends State<MyHomePage> {
     if (displayExOh[0] == displayExOh[1] &&
         displayExOh[0] == displayExOh[2] &&
         displayExOh[0] != '') {
-      checkWinnerDialog();
+      checkWinnerDialog(displayExOh[0]);
     }
     //2-qator
     if (displayExOh[3] == displayExOh[4] &&
         displayExOh[3] == displayExOh[5] &&
         displayExOh[3] != '') {
-      checkWinnerDialog();
+      checkWinnerDialog(displayExOh[3]);
     }
     //3-qator
     if (displayExOh[6] == displayExOh[7] &&
         displayExOh[6] == displayExOh[8] &&
         displayExOh[6] != '') {
-      checkWinnerDialog();
+      checkWinnerDialog(displayExOh[6]);
     }
     //1-qator
     if (displayExOh[0] == displayExOh[3] &&
         displayExOh[0] == displayExOh[6] &&
         displayExOh[0] != '') {
-      checkWinnerDialog();
+      checkWinnerDialog(displayExOh[0]);
     }
     //2-qator
     if (displayExOh[1] == displayExOh[4] &&
         displayExOh[1] == displayExOh[7] &&
         displayExOh[1] != '') {
-      checkWinnerDialog();
+      checkWinnerDialog(displayExOh[1]);
     }
     //3-qator
     if (displayExOh[2] == displayExOh[5] &&
         displayExOh[2] == displayExOh[8] &&
         displayExOh[2] != '') {
-      checkWinnerDialog();
+      checkWinnerDialog(displayExOh[2]);
     }
     //1-dioganal
     if (displayExOh[0] == displayExOh[4] &&
         displayExOh[0] == displayExOh[8] &&
         displayExOh[0] != '') {
-      checkWinnerDialog();
+      checkWinnerDialog(displayExOh[0]);
     }
     //2-dioganal
     if (displayExOh[2] == displayExOh[4] &&
         displayExOh[2] == displayExOh[6] &&
         displayExOh[2] != '') {
-      checkWinnerDialog();
+      checkWinnerDialog(displayExOh[2]);
+    } else if (filledBoxes == 9) {
+      _showDrawDialog();
     }
   }
 
-  void checkWinnerDialog() {
+  void _showDrawDialog() {
     showDialog(
+      barrierDismissible: false,
       context: context,
       builder: (BuildContext context) {
-        return AlertDialog(title: Text('Winner is: ' + (ohTurn ? 'X' : 'O')));
+        return AlertDialog(
+          actions: [
+            TextButton(
+              onPressed: () {
+                clearBoard();
+                Navigator.of(context).pop();
+              },
+              child: Text("Qayta o'ynash"),
+            ),
+          ],
+          title: Text("Durang!"),
+        );
       },
     );
+  }
+
+  void checkWinnerDialog(String winner) {
+    showDialog(
+      barrierDismissible: false,
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          actions: [
+            TextButton(
+              onPressed: () {
+                clearBoard();
+                Navigator.of(context).pop();
+              },
+              child: Text("Qayta o'ynash"),
+            ),
+          ],
+          title: Text("G'olib: ${ohTurn ? 'X' : 'O'}"),
+        );
+      },
+    );
+    if (winner == 'X') {
+      playerXscore++;
+    } else if (winner == 'O') {
+      playerOscore++;
+    }
+  }
+
+  void clearBoard() {
+    setState(() {
+      for (int i = 0; i < 9; i++) {
+        displayExOh[i] = '';
+      }
+      filledBoxes = 0;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[800],
-      body: GridView.builder(
-        itemCount: 9,
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 3,
-        ),
-        itemBuilder: (BuildContext context, index) {
-          return GestureDetector(
-            onTap: () {
-              _tapped(index);
-            },
-            child: Container(
-              decoration: BoxDecoration(border: Border.all(color: Colors.grey)),
-              child: Center(
-                child: Text(
-                  displayExOh[index],
-                  style: TextStyle(color: Colors.white, fontSize: 40),
-                ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "O'yinchi: O",
+                        style: TextStyle(color: Colors.white, fontSize: 24),
+                      ),
+                      Text(
+                        playerOscore.toString(),
+                        style: TextStyle(color: Colors.white, fontSize: 24),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "O'yinchi: X",
+                        style: TextStyle(color: Colors.white, fontSize: 24),
+                      ),
+                      Text(
+                        playerXscore.toString(),
+                        style: TextStyle(color: Colors.white, fontSize: 24),
+                      ),
+                    ],
+                  ),
+                ],
               ),
             ),
-          );
-        },
+          ),
+          Expanded(
+            flex: 3,
+            child: GridView.builder(
+              itemCount: 9,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+              ),
+              itemBuilder: (BuildContext context, index) {
+                return GestureDetector(
+                  onTap: () {
+                    _tapped(index);
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey),
+                    ),
+                    child: Center(
+                      child: Text(
+                        displayExOh[index],
+                        style: TextStyle(color: Colors.white, fontSize: 40),
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+          Expanded(child: Container()),
+        ],
       ),
     );
   }
